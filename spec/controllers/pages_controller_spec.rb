85f9ef95cc217f62a965e14de07eabbb121bbccf
Yannan Wang <yannanwang@gmail.com>
2011-07-07 11:46:54 +0800

    Fixed Spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,7 +10,7 @@ describe PagesController do
     end
 
     it "should have the right title" do
-      get 'home'r
+      get 'home'
       response.should have_selector("title",
                         :content => "Ruby on Rails Tutorial Sample App | Home")
     end
